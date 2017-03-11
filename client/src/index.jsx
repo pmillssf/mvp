ed8c0ba93f466cd906a29203c097cdc10a058ce3
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-10 16:19:29 -0800

    Outline basic app in index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Hexagon from 'react-hexagon'
+import Hexagon from 'react-hexagon';
 
-ReactDom.render(<Hexagon style={{stroke: '#42873f'}}/>, document.getElementById('app'));
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return <div>
+			<h1>Welcome</h1>
+			<h3>Game Rules</h3>
+			<div>
+				<input value='Username_1'></input>
+				<input value='Username_2'></input>
+				<button>start game</button>
+				<button>Load save</button>
+			</div>
+			<div>
+				<div>
+					<div>Player Turn <button>save</button></div>
+				</div>
+				<div>HexRow_1</div>
+				<div>HexRow_2</div>
+				<div>HexRow_3</div>
+				<div>HexRow_4</div>
+				<div>HexRow_5</div>
+			</div>
+		</div>
+	}
+}
+
+ReactDom.render(<App/>, document.getElementById('app'));
