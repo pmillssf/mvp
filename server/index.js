24ef1fadeca4a8ee599d6355d1b0836ea83f1678
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-10 16:39:27 -0800

    Setup server/index.js for app testing

---
+++ server/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join(__dirname, '../client/dist/')))
+
+app.listen(3000, function(){
+	console.log('App is listening on port 3000!', __dirname)
+})
