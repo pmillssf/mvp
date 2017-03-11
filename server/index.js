ca453e34eba88b4a7a42f4060ae9a9201cee9991
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-10 16:58:55 -0800

    Clean up server/index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname, '../client/dist/')))
+app.use(express.static(path.join(__dirname, '../client/dist/')));
 
-app.listen(3000, function(){
-	console.log('App is listening on port 3000!', __dirname)
-})
+app.listen(3000, function() {
+	console.log('App is listening on port 3000!');
+});
