c5c4e0d479354dee6afd86c1a9cb77a68e6ed7b4
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-11 15:39:53 -0800

    Establish mysql database connection

---
+++ db/index.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'hex'
+});
+
+connection.connect();
+
+function getUsername(username, callback) {
+	connection.query('SELECT users.username FROM users WHERE users.username = ?', [username], function(err, results) {
+		if (err) {
+			callback(err, null);
+		} else {
+			callback(null, results);
+		}
+	})
+}
+
+module.exports.getUsername = getUsername;
