42503456f34ce9c1835375e56b1d54038b5b16fb
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-03-11 23:36:43 -0800

    Render board on clicking new game button

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,9 +10,11 @@ class App extends React.Component {
 		this.state = {
 			playerOne: 'Red',
 			playerTwo: 'Blue',
+			startGame: false
 		}
 		this.playerOneChange = this.playerOneChange.bind(this);
 		this.playerTwoChange = this.playerTwoChange.bind(this);
+		this.renderGame = this.renderGame.bind(this);
 	}
 	playerOneChange(e) {
 		this.setState({
@@ -24,6 +26,11 @@ class App extends React.Component {
 			playerTwo: e.target.value
 		})
 	}
+	renderGame() {
+		this.setState({
+			startGame: !this.state.startGame
+		})
+	}
 	render() {
 		return <div>
 			<h1>Welcome</h1>
@@ -33,10 +40,10 @@ class App extends React.Component {
 				<input value={this.state.playerTwo} onChange={this.playerTwoChange} maxLength={10}></input>
 			</div>
 			<div>
-				<button>New Game</button>
+				<button onClick={this.renderGame}>New Game</button>
 				<button>Load Game</button>
 			</div>
-			<HexBoard />
+			{this.state.startGame ? <HexBoard playerOne={this.state.playerOne} playerTwo = {this.state.playerTwo}/> : ''}
 		</div>
 	}
 }
